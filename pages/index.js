33142a05dd19bde0ba45371bab8fc3dd2197555e
Thananjaya <thananjaya091993@gmail.com>
2019-03-02 17:53:32 +0530

    added getInitialProps and ant design

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,17 @@ import instance from '../ethereum/web3/factory';
 
 class CrowdFundingIndex extends Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            deployedCrowdFundings: [],
-        }
-    }
-
-    async componentWillMount(){
-        this.state.deployedCrowdFundings.push(await instance.methods.getDeployedContractAddresses().call());
+    static async getInitialProps() {
+        const crowdFundings = await instance.methods.getDeployedContractAddresses().call();
+        return { crowdFundings }
     }
 
     render(){
         return(
             <div>
                 <h1>Crowd Funding Index</h1>
+                <p>Deployed CrowdFunding address are</p>
+                {this.props.crowdFundings[0]}
             </div>
         )
     }
