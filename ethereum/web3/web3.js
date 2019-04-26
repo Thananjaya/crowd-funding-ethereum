337668ecbf50430f1d2c77dc2b0254a5cfd1b2c9
Thananjaya <thananjaya091993@gmail.com>
2019-04-26 10:49:25 +0530

    added infura api config

--- ethereum/web3/web3.js
+++ ethereum/web3/web3.js
@@ -2,14 +2,12 @@ const Web3 = require('web3');
 
 let web3;
 
-// checking whether the process is running in the browser or server
 if( typeof window !== 'undefined' && typeof window.web3 !== 'undefined' ) {
-    // process running in the browser and browser has metamask
     web3 = new Web3(window.web3.currentProvider);
 } else {
     // process is not running in browser or not having the metamask plugin
     const provider = new Web3.providers.HttpProvider(
-        // YOUR INFURA PROVIDER API
+        "https://rinkeby.infura.io/v3/ffd2deb0df864ae299640e5d442c4eaa"
     );
     web3 = new Web3(provider);
 }
